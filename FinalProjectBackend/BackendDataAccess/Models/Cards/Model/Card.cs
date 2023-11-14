2f8d5d3c6f7f1ceee3ee6511211ecf7c12ef96d1
Mark Stone <markstone1234@hotmail.com>
2023-11-14 10:16:21 +0100

    test

---
+++ FinalProjectBackend/BackendDataAccess/Models/Cards/Model/Card.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendDataAccess.Models.Cards.Model
+{
+    public class Card
+    {
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("categoryId")]
+        public string CategoryId;
+
+        [JsonProperty("title")]
+        public string Title;
+
+        [JsonProperty("wrong-answer")]
+        public string WrongAnswer;
+
+        [JsonProperty("answers")]
+        public List<Answer> Answers;
+
+    }
+}
